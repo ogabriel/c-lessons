d9936950240b79847fc7d4805a79a4438099b899
ogabriel <gabrieloliver8991@gmail.com>
2019-06-03 22:45:29 -0300

    add sc static

---
+++ storage_class_static.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+void func(void);
+static int count = 5;
+
+int main() {
+    // static storage class
+    // tells the compiler to keep the local variable in existence during the life-time of the program
+    // instead of creating and destroyingit each time it comes into and goes out of scope
+    // therefore, make static variables, allows to maintain their values between functions calls
+    // static modifier may be applied to global variables, when this is done
+    // it causes the variable scope to be restricted to the file which is declareted
+    // it calses only one copy of that data member to be shared by all the objects of that class
+
+    while(count--)
+        func();
+
+    return 0;
+}
+
+void func(void) {
+    static int i = 5;
+    i++;
+    printf("i is %d and count is %d\n", i, count);
+}
