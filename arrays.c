e3d56219e515cbd93dee4208415e066c31a36fa5
ogabriel <gabrieloliver8991@gmail.com>
2019-06-06 23:55:40 -0300

    add arrays

---
+++ arrays.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    // declare a array
+    // data_type name [const array size]
+    int balance[5] = { 2, 6, 7, 8};
+    int balance2[5][2] = {{1, 2}, {1, 2, 4}};
+
+    return 0;
+}
