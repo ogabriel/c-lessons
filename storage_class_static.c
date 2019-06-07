87e737650cca8f0ef634d1416f64a77c9d3fc987
ogabriel <gabrieloliver8991@gmail.com>
2019-06-06 23:54:35 -0300

    update

--- storage_class_static.c
+++ storage_class_static.c
@@ -6,7 +6,7 @@ static int count = 5;
 int main() {
     // static storage class
     // tells the compiler to keep the local variable in existence during the life-time of the program
-    // instead of creating and destroyingit each time it comes into and goes out of scope
+    // instead of creating and destroying it each time it comes into and goes out of scope
     // therefore, make static variables, allows to maintain their values between functions calls
     // static modifier may be applied to global variables, when this is done
     // it causes the variable scope to be restricted to the file which is declareted
